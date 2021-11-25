fb9e91359b55e26c0ea6cc0fe02a36b87f91dc18
Blexx10 <92892195+Blexx10@users.noreply.github.com>
2021-11-25 16:12:37 -0500

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 def on_forever():
-    pass
+    while True:
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        basic.pause(5000)
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        basic.pause(2000)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P1, 1)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,5 +6,8 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.pause(2000)
         pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
     }
 })
